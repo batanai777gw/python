a28843b8981fd024a7aba26278ecc42b69d56390
batanai777gw <batanai178@gmail.com>
2023-08-01 19:10:01 +0200

    'variable:stringmethods

---
+++ strings.py
@@ -0,0 +1,19 @@
+# Using python string methods
+
+# Asking user for their name
+name = input("What's your name? ")
+
+# Remove whitespace from string
+name = name.strip()
+
+# Capitilize user's name
+sname = name.upper()
+
+print(f"hello, {name}")
+
+
+"""
+    To do List:
+    10 python string method problem solving exercises online
+
+"""
